7e9317294b878d763ed870161c78e7d2b044901e
Bakr Khalil <demlyoafrin@gmail.com>
2025-10-13 12:43:38 +0200

    head class

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TjuvOchPolis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
