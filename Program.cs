96feeb58e039267a63e772079b3c952942210fe1
Bakr <demlyoafrin@gmail.com>
2025-10-14 13:43:44 +0200

    hej

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace TjuvOchPolis
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------");
+            Console.WriteLine("...............");
             
         }
     }
