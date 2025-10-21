9048b248e2306788e2230dfac0144730dbe950cb
Hussein Mumin Awbakar <Husseinmumin97@gmail.com>
2025-10-21 13:51:25 +0200

    uppgrade

---
+++ Interaction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis
+{
+    internal class Interaction
+    {
+        public static void CheckInteration(Citizen citizen, Thief thief, Police police, List<string> newsFeed)
+        {
+            if (thief.X == citizen.X && thief.Y == citizen.Y)
+            {
+                if (citizen.Inventory.Count > 0)
+                {
+                    int index = Random.Shared.Next(citizen.Inventory.Count);
+                    string item = citizen.Inventory[index];
+
+                    citizen.Inventory.RemoveAt(index);
+                    thief.Inventory.Add(item);
+
+                    newsFeed.Add($"Tjuv rånar medborgare på {item}!");
+                }
+            }
+
+            if (police.X == thief.X && police.Y == thief.Y)
+            {
+                police.Inventory.AddRange(thief.Inventory);
+                thief.Inventory.Clear();
+
+                newsFeed.Add("Polis tar tjuv!");
+            }
+
+            if (police.X == citizen.X && police.Y == citizen.Y)
+            {
+                newsFeed.Add("Polis hälsar på medborgare!");
+            }
+
+            if (newsFeed.Count > 4)
+                newsFeed.RemoveAt(0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using System.Threading;
 
 namespace TjuvOchPolis
 {
@@ -7,33 +9,98 @@ namespace TjuvOchPolis
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             City.DrawCity();
             List<Person> people = new List<Person>();
-            
+            List<string> newsFeed = new List<string>();
+
+
+
+
+            for (int i = 0; i < 10; i++) // skapar poliser
+
+                people.Add(new Police("P", Random.Shared.Next(2, 99), Random.Shared.Next(2, 23)));
+
+            for (int i = 0; i < 20; i++) // skapar tjubar
+
+                people.Add(new Thief("T", Random.Shared.Next(2, 99), Random.Shared.Next(2, 23)));
+
+            for (int i = 0; i < 30; i++) // skapar medborgare
+
+                people.Add(new Citizen("M", Random.Shared.Next(2, 99), Random.Shared.Next(2, 23)));
+
+
 
 
             while (true)
             {
-              
-                Console.ForegroundColor = ConsoleColor.Red;
-                //people.Add(new Police("P", Random.Shared.Next(1, 99), Random.Shared.Next(1, 23)));
-                people.Add(new Police("P",5, 0, 0));
-                people.Add(new Thief("T",10, 0, 0));
-                people.Add(new Citizen("M",20, 0, 0));
-                
 
-                foreach (var person in people)
+                foreach (Person person in people)
                 {
                     Console.SetCursorPosition(person.X, person.Y);
-                    Console.WriteLine(person.Name);
-                    
+                    Console.Write(" "); // Radera personens nuvarande position
+
+
+
+                    if (person.X <= 1 || person.X >= 98)
+                    {
+                        person.Xdirection *= -1; // Vänd riktning horisontellt
+                    }
+                    if (person.Y <= 1 || person.Y >= 23)
+                    {
+                        person.Ydirection *= -1; // Vänd riktning vertikalt
+                    }
+
+                    person.X += person.Xdirection;
+                    person.Y += person.Ydirection;
+
+                    Console.SetCursorPosition(person.X, person.Y);
+                    Console.ForegroundColor = person switch
+                    {
+                        Police => ConsoleColor.Blue,
+                        Thief => ConsoleColor.Red,
+                        Citizen => ConsoleColor.Green,
+                        _ => ConsoleColor.White
+
+
+                    };
+
+                    Console.Write(person.Name); // Rita personen på den nya positionen
                 }
-                Thread.Sleep(1500);
-                break; // ta bort denna rad för att låta loopen fortsätta
+
+                foreach (Person a in people)
+                {
+                    foreach (Person b in people)
+                    {
+                        foreach (Person c in people)
+                        {
+                            if (a != b && b != c && a != c)
+                            {
+                                if (a is Citizen citizen && b is Thief thief && c is Police police)
+                                {
+                                    Interaction.CheckInteration(citizen, thief, police, newsFeed);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < newsFeed.Count; i++)
+                {
+                    Console.SetCursorPosition(0, 41 + i);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(newsFeed[i].PadRight(100));
+                }
+
+
+
+                Thread.Sleep(1000);
+                //break; // ta bort denna rad för att låta loopen fortsätta
             }
             Console.ReadKey();
-
-
         }
+        
+       
     }
+
 }
